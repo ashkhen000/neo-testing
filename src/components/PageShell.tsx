5738b901cf7884dc968b6114852c47aa56a03d87
ashkhen000 <ashkhen978@gmail.com>
2026-06-07 11:56:03 +0400

    Update PageShell.tsx

--- src/components/PageShell.tsx
+++ src/components/PageShell.tsx
@@ -4,9 +4,11 @@ import { LoadingScreen } from "./LoadingScreen";
 
 export function PageShell({ children }: { children: React.ReactNode }) {
   return (
-    <LoadingScreen>
+       <LoadingScreen>
       <div className="min-h-screen flex flex-col">
+        <SiteHeader />
         <main className="flex-1">{children}</main>
+        <SiteFooter />
       </div>
     </LoadingScreen>
   );
