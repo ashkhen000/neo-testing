c06ca587f66f859b9d32a3e291aa662b86a9bcfd
ashkhen000 <ashkhen978@gmail.com>
2026-06-07 10:41:39 +0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RootLayout, NotFoundComponent } from "@/components/RootLayout";
+import HomePage from "@/pages/HomePage";
+import AboutPage from "@/pages/AboutPage";
+import ContactPage from "@/pages/ContactPage";
+import ServicesLayout from "@/pages/ServicesLayout";
+import ServicesIndexPage from "@/pages/ServicesIndexPage";
+import WindowsPage from "@/pages/WindowsPage";
+import DoorsPage from "@/pages/DoorsPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          {/* Main Top-Level Routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+
+          {/* Nested Services Hierarchy with Layout Wrapper */}
+          <Route path="/services" element={<ServicesLayout />}>
+            <Route index element={<ServicesIndexPage />} />
+            <Route path="windows" element={<WindowsPage />} />
+            <Route path="doors" element={<DoorsPage />} />
+          </Route>
+
+          {/* Fallback 404 Route */}
+          <Route path="*" element={<NotFoundComponent />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
